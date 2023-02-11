cee023b8617dd06f28fb767bff7ca2317e79f857
danilo-silva-cruz <danilocruz.adm@gmail.com>
2023-02-11 13:51:41 -0300

    Alterado de inglês para português

--- src/exercise/Exercise001.java
+++ src/exercise/Exercise001.java
@@ -2,6 +2,6 @@ package exercise;
 
 public class Exercise001 {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        System.out.println("Olá, mundo!");
     }
 }
